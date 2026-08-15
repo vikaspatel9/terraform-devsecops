f3cf2ad741cb3801f65f8c57967d04cd0eff0f4b
Vikas Chandra Patel <vikaspatel9>
2026-08-15 13:10:41 +0530

    rg

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "5.0.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {}
+  
+}
+resource "azurerm_resource_group" "vikas" {
+    name = "rg-1"
+    location = "eastus"
+  
+}
