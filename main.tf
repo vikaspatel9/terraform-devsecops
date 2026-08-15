f0362fe21a00c75ef8e9c45b9bd19c8ccd52cdda
Vikas Chandra Patel <vikaspatel9>
2026-08-15 13:52:03 +0530

    fix: format terraform configuration

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "5.0.0"
+      source  = "hashicorp/azurerm"
+      version = "5.0.0"
     }
   }
 }
 provider "azurerm" {
-    features {}
-  
+  features {}
+
 }
 resource "azurerm_resource_group" "vikas" {
-    name = "rg-1"
-    location = "eastus"
-  
+  name     = "rg-1"
+  location = "eastus"
+
 }
